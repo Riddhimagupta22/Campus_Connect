85b9b5446b14ee2990740d0a4a3e27b41dd18db5
Riddhima Gupta <griddhima226@gmail.com>
2025-10-02 14:13:13 +0530

    splash screen

--- android/app/src/main/kotlin/com/example/campus_connects/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/campus_connects/MainActivity.kt
@@ -2,4 +2,6 @@ package com.example.campus_connects
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity(){
+
+}
--- lib/modules/splash_screen.dart
+++ lib/modules/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -7,8 +10,56 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        fit: StackFit.expand,
         children: [
-          Image(image: AssetImage("Assets/Logo/slashlogo.jpg"),fit: BoxFit.fill,)
+          Image(image: AssetImage("Assets/Logo/slashlogo.jpg"),
+            fit: BoxFit.fill,
+            colorBlendMode: BlendMode.modulate ,),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0), // Adjust blur intensity
+            child: Container(
+              color: Colors.black.withOpacity(0), // Transparent overlay
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 109,
+                  width: 110,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [Color(0xFF2196F3),Color(0xFF8E24AA)],
+                  begin: Alignment.centerLeft,end: Alignment.centerRight),
+                  borderRadius:BorderRadius.circular(25)
+                ),
+              ),
+              Text("Campus Connect",
+                style: GoogleFonts.inter(
+                    fontSize:26,fontWeight:FontWeight.w800,
+                    color: Color(0xFF282727)),),
+              Text("Beyond One Campus",
+                style: GoogleFonts.inter(
+                    fontSize:18,fontWeight:FontWeight.w600,
+                    color: Color(0xFF454141)),),
+
+              SizedBox(height: 60),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 5,
+                    backgroundColor: Color(0xFF2196F3),
+                  ),
+                  SizedBox(width: 6),
+                  CircleAvatar(radius: 5, backgroundColor: Color(0xFF9C27B0),),
+                  SizedBox(width: 6),
+                  CircleAvatar(radius: 5, backgroundColor: Color(0xFFE91E63),
+                  )
+                ],
+              ),
+            ],
+          )
         ],
       ),
     );
